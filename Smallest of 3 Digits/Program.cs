31b227f8062f4e0f85fa32ecd3d40c0d71270892
SamirAliyevv <samirsa@code.edu.az>
2023-01-16 13:54:31 -0800

    master

---
+++ Smallest of 3 Digits/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Smallest_of_3_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 14;
+            int b = 4;
+            int c = 4;
+            if (a >= b && a >= c)
+            {
+                Console.WriteLine("A ededi en boyuk ededdir");
+            }
+            else if (b >= c && b >= a)
+            {
+
+                Console.WriteLine(" B ededi en boyuk ededdir");
+            }
+            else
+            {
+                Console.WriteLine("C  ededi en boyuk ededdir");
+            }
+        }
+    }
+}
